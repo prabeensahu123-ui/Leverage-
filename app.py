3f9da4ff0bf0da37d69c019d4c7a22afa5984dea
prabeensahu123-ui <prabeensahu123@gmail.com>
2026-08-21 23:17:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=u
 response = requests.get(url).json()
 
 if "bitcoin" in response:
-    btc_price = response["bitcoin"]["usd"]
-    btc_change = response["bitcoin"]["usd_24hr_change"]
+    btc_price = response["bitcoin"].get("usd", 0)
+    btc_change = response["bitcoin"].get("usd_24hr_change", 0)
     
+    # Fallback if change is None
+    if btc_change is None:
+        btc_change = 0.0
+
     st.metric(label="Bitcoin Price (USD)", value=f"${btc_price:,.2f}", delta=f"{btc_change:.2f}%")
 else:
     st.write("Could not fetch live data at the moment. Please refresh!")
